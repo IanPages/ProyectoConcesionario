07a55ef94e2c98e2deab37ec6aeee1a6274b1bae
IanPages <ianpagesrodriguez@gmail.com>
2024-05-15 11:55:31 +0100

    DB

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -1,7 +1,7 @@
 import Sequelize  from 'sequelize';
 
 export const sequelize = new Sequelize(
-    'concesionario','root','',
+    'concesionario','root','1234',
     {
         host: 'localhost',
         dialect: 'mysql'
